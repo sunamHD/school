0f2a1adecc90732e3fc0e4e91e5b259e1ec8c716
destiny <destiny@doom.machine>
2014-08-15 01:20:01 -0400

    Dynamic name viewing and automatic change of breakdance to 1 or 0

--- application/views/students/create.php
+++ application/views/students/create.php
@@ -2,16 +2,20 @@
 
 <?php echo validation_errors(); ?>
 
-<?php echo form_open('students/create') ?>
+<h3>Name (as per current input):<span id="nameDisp"></span></h3>
+<?php 
+    $attributes = array('id' => 'createStudentForm');
+    echo form_open('students/create', $attributes) 
+?>
 
 	<label for="firstName">First Name</label>
-	<input type="input" name="firstName" /><br />
+	<input type="input" name="firstName" onkeyup="updateNameDisplay()" /><br />
 
 	<label for="midName">Middle Name</label>
-	<input type="input" name="midName" /><br />
+	<input type="input" name="midName" onkeyup="updateNameDisplay()"/><br />
 
 	<label for="lastName">Last Name</label>
-	<input type="input" name="lastName" /><br />
+	<input type="input" name="lastName" onkeyup="updateNameDisplay()"/><br />
 
 	<label for="year">Year (e.g. Sophomore)</label>
 	<input type="input" name="year" /><br />
@@ -22,6 +26,26 @@
 	<label for="breakdance">Is a breakdancer? (1 for yes, 0 for no)</label>
 	<input type="input" name="breakdance" /><br />
 
-	<input type="submit" name="submit" value="Create student" />
+	<input type="submit" name="submit" value="Create student" onclick="breakFix()" />
 
 </form>
+<script>
+    function updateNameDisplay(){
+        var first = document.getElementsByName('firstName')[0].value;
+        var mid = document.getElementsByName('midName')[0].value;
+        var last = document.getElementsByName('lastName')[0].value;
+        document.getElementById('nameDisp').innerHTML = ' ' + first + ' ' +  mid + ' ' + last;
+    }
+</script>
+
+<script>
+    function breakFix(){
+        var breaker = document.getElementsByName('breakdance')[0].value;
+        if (breaker == 'y'){
+            document.getElementsByName('breakdance')[0].value = 1;
+        }
+        else if (breaker == 'n'){
+            document.getElementsByName('breakdance')[0].value = 0;
+        }
+    }
+</script>
